d7d1bc74224f67f07932d5b2d9dc30bb521b55f5
paramjeetsingh2000 <paramjeetsinghjawla99@gmail.com>
2025-11-02 01:27:10 +0530

    implemented cube throw logic

--- Assets/Scripts/CubeManager.cs
+++ Assets/Scripts/CubeManager.cs
@@ -1,16 +1,87 @@
 using UnityEngine;
 
-public class CubeManager : MonoBehaviour
+public class CubeManager : MonoBehaviour 
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static CubeManager Instance { get; private set; }
+    [SerializeField] private Transform cubePos;
+    [SerializeField] private Cube cube, currentCube;
+    [SerializeField] private float slideSpeed = 0.01f, force;
+
+    private Vector2 lastPointerPosition;
+    private bool isDragging = false;
+
+    private void Awake()
     {
-        
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    private void Start()
+    {
+        AddCube();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerDown()
     {
-        
+        if (Input.touchCount > 0)
+        {
+            lastPointerPosition = Input.touches[0].position;
+            isDragging = true;
+        }
+        else
+        {
+            lastPointerPosition = Input.mousePosition;
+            isDragging = true;
+        }
+    }
+
+    public void OnPointerUp()
+    {
+        isDragging = false;
+        currentCube.GetComponent<Rigidbody>().AddForce(Vector3.forward * force, ForceMode.Impulse);
+        Invoke("AddCube", 0.3f);
+    }
+
+    private void Update()
+    {
+        if (isDragging)
+        {
+            if (Input.touchCount > 0)
+            {
+                currentCube.transform.position += new Vector3((lastPointerPosition.x - Input.touches[0].position.x) * -slideSpeed * Time.deltaTime, 0f, 0f);
+                lastPointerPosition = Input.touches[0].position;
+                if (currentCube.transform.position.x > 1.12)
+                {
+                    currentCube.transform.position = new Vector3(1.12f, currentCube.transform.position.y, currentCube.transform.position.z);
+                }
+                else if (currentCube.transform.position.x < -1.12)
+                {
+                    currentCube.transform.position = new Vector3(-1.12f, currentCube.transform.position.y, currentCube.transform.position.z);
+                }
+            }
+            else
+            {
+                currentCube.transform.position += new Vector3((lastPointerPosition.x - Input.mousePosition.x) * -slideSpeed * Time.deltaTime, 0f, 0f);
+                lastPointerPosition = Input.mousePosition;
+                if (currentCube.transform.position.x > 1.12)
+                {
+                    currentCube.transform.position = new Vector3(1.12f, currentCube.transform.position.y, currentCube.transform.position.z);
+                }
+                else if (currentCube.transform.position.x < -1.12)
+                {
+                    currentCube.transform.position = new Vector3(-1.12f, currentCube.transform.position.y, currentCube.transform.position.z);
+                }
+            }
+        }
+    }
+    public void AddCube()
+    {
+        currentCube = Instantiate(cube,cubePos.position, Quaternion.identity);
     }
 }
